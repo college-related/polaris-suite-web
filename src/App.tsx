18f6f54242fe360b97b8719dea68f50feab17229
Prasiddha Khadka <be2018se630@gces.edu.np>
2023-04-23 15:00:36 +0545

    resolving layout conversation

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,23 @@ import DocsPage from "./pages/DocsPage";
 function App() {
   return (
     <BrowserRouter>
-      <MasterLayout>
-        <Routes>
-          <Route path="/" element={<HeroSection />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/docs" element={<DocsPage />} />
-        </Routes>
-      </MasterLayout>
+      <Routes>
+        <Route path="/*" element={<PublicRoutes />} />
+        <Route path="/docs" element={<DocsPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
+
+const PublicRoutes = () => {
+  return (
+    <MasterLayout>
+      <Routes>
+        <Route path="/" element={<HeroSection />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </MasterLayout>
+  );
+};
 
 export default App;
