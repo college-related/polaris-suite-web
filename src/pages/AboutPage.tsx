006f7ccff91081df5766587db246c5d0dddf9f5c
alsongarbuja <garbuja12alson@gmail.com>
2023-05-09 19:18:30 +0545

    update-page: update about page our story section

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,7 +1,5 @@
-import React from "react";
 import {
   auto_test,
-  bg,
   collab_test,
   github_logo,
   unit_test,
@@ -10,8 +8,8 @@ import {
 const AboutPage = () => {
   return (
     <div>
-      <div className="about_header">
-        <div className="heading_text flex flex-col items-center">
+      <div className="about_header pb-12">
+        <div className="heading_text flex flex-col items-center mt-20">
           <p className="font-sen text-h3 font-bold sm:text-h1">
             Automate Test with Ease
           </p>
@@ -23,109 +21,35 @@ const AboutPage = () => {
         </div>
       </div>
 
-      <div className="our_story_section w-full h-auto mt-32">
-        <div className="outer_container flex flex-col items-center">
-          <img
-            src={bg}
-            alt="bg_main"
-            className="absolute w-72 top-52 ss:top-36 smd:top-28  md:top-28 sm:w-1/3 mt-4 sm:mt-44 lg:top-24"
-          />
-          <div className="container">
-            <div className="bg-gradient-to-b from-indigo-300 to-white w-full h-auto sm:h-screen">
-              <div
-                className="bg_img w-full h-auto flex items-center justify-center flex-col
-              
-              "
-              >
-                <div className="bg-bg bg-center bg-contain bg-repeat-x sm:bg-center sm:bg-cover sm:h-screen">
-                  <div className="flex flex-col items-center pt-20 px-9 md:mt-36">
-                    <p className="font-sen text-h3 mt-2">Our Story</p>
-                    <p className="font-quicksand sm:w-2/3 text-center pb-5">
-                      Polaris Suite is a final year project build by{" "}
-                      <b>Alson Garbuja</b>, <b>Salipa Gurung</b> and
-                      <b> Prasiddha Khadka</b>. A group of three student
-                      studying Software Engineering at Gandaki College of
-                      Engineering and Science.
-                    </p>
-                    <p className="font-quicksand sm:w-2/3 text-center">
-                      It is important academic endeavour for us while pursuing
-                      degrees in information technology or related fields. This
-                      project typically involves in-depth research, analysis,
-                      design, implementation, and evaluation of a testing
-                      application. We have applied all the theoretical concepts
-                      and practical skills learned throughout their academic
-                      journey to solve real-world problems.It also provides an
-                      opportunity for upcoming engineers to analyze and learn
-                      from our project.
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="bg-bg text-center py-5 h-[700px] flex flex-col justify-center">
+          <p className="text-h4 font-bold font-sen">Our Story</p>
+          <p className="w-3/4 mx-auto my-4">
+            We are a group of 3 students from Gandaki College Of Engineering and Science who are
+            passionate about software development. We have been working on
+            various projects and have been using various testing tools. We
+            realized that there is a need for a tool that can automate the
+            testing process and make it easier for developers to collaborate and test their
+            code with testers and other stakeholders of the project/product. So, as a major project
+            for our final year we decided to build a tool that can automate the testing
+            process and make it easier for developers, testers, and stakeholders to collaborate and test their product.
+          </p>
+          <p className="w-3/4 mx-auto">
+            Polaris suite is a tool that can automate the testing process and make it easier for developers, testers, and stakeholders to collaborate and test their product.
+            It has all the necessary tools and features that are required for testing a product. It has a unit testing tool, a collaborative testing tool, and an automation testing tool.
+            Along side the package, we are also offering web and mobile application that can be used to manage the testing process and view the test results.
+          </p>
       </div>
 
       {/* Below Section */}
-      <div className="px-8 pt-10 flex justify-start items-center sm:mx-10 ">
-        <p className="font-sen text-h3 ss:text-2xl text- font-bold">Our Team</p>
-      </div>
-
-      <div className="font-quicksand bg-gray-100 flex flex-col sm:flex-row sm:items-baseline space-y-7 justify-around items-center pt-12 pb-12 sm:flex">
-        {/*  */}
-        <div className="max-w-xs container bg-white rounded-xl shadow-lg transform transition duration-500 hover:scale-105 hover:shadow-2xl">
-          <div className="flex items-center justify-between">
-            <h1 className="text-2xl mt-2 ml-4 font-bold text-gray-800 cursor-pointer hover:text-gray-900 transition duration-100 pt-5 pb-4">
-              Alson
-              <br />
-              Garbuja
-            </h1>
-            <a href="https://github.com/alsongarbuja" target="_blank">
-              <img src={github_logo} alt="" className="w-10 h-10 mr-5" />
-            </a>
-          </div>
-          <img
-            className="w-full cursor-pointer"
-            src="https://avatars.githubusercontent.com/u/42911859?v=4"
-            alt="alson_img"
-          />
+      <div className="font-quicksand bg-gray-100 pt-12 pb-12 ">
+        <div className="pl-8 sm:mx-10">
+          <p className="font-sen text-h3 ss:text-2xl font-bold">Our Team</p>
         </div>
-
         {/*  */}
-        <div className="max-w-xs container bg-white rounded-xl shadow-lg transform transition duration-500 hover:scale-105 hover:shadow-2xl">
-          <div className="flex items-center justify-between">
-            <h1 className="text-2xl mt-2 ml-4 font-bold text-gray-800 cursor-pointer hover:text-gray-900 transition duration-100 pt-5 pb-4">
-              Salipa
-              <br />
-              Gurung
-            </h1>
-            <a href="https://github.com/salipa-gurung" target="_blank">
-              <img src={github_logo} alt="" className="w-10 h-10 mr-5" />
-            </a>
-          </div>
-          <img
-            className="w-full cursor-pointer"
-            src="https://avatars.githubusercontent.com/u/53458341?v=4"
-            alt="salipa_img"
-          />
-        </div>
-
-        {/*  */}
-        <div className="max-w-xs container bg-white rounded-xl shadow-lg transform transition duration-500 hover:scale-105 hover:shadow-2xl">
-          <div className="flex items-center justify-between">
-            <h1 className="text-2xl mt-2 ml-4 font-bold text-gray-800 cursor-pointer hover:text-gray-900 transition duration-100 pt-5 pb-4">
-              Prasiddha <br />
-              Khadka
-            </h1>
-            <a href="https://github.com/Prasiddha777" target="_blank">
-              <img src={github_logo} alt="" className="w-10 h-10 mr-5" />
-            </a>
-          </div>
-          <img
-            className="w-full cursor-pointer"
-            src="https://avatars.githubusercontent.com/u/53443661?v=4"
-            alt="prasid_img"
-          />
+        <div className="flex flex-col sm:flex-row sm:items-baseline space-y-7 justify-around items-center sm:flex">
+          <TeamCard firstname="Alson" lastname="Garbuja" username="alsongarbuja" github_profile_url="https://avatars.githubusercontent.com/u/42911859?v=4" />          
+          <TeamCard firstname="Salipa" lastname="Gurung" username="salipa-gurung" github_profile_url="https://avatars.githubusercontent.com/u/53458341?v=4" />          
+          <TeamCard firstname="Prasiddha" lastname="Khadka" username="Prasiddha777" github_profile_url="https://avatars.githubusercontent.com/u/53443661?v=4" />          
         </div>
       </div>
 
@@ -153,5 +77,34 @@ const AboutPage = () => {
     </div>
   );
 };
+
+interface TeamCardProps {
+  firstname: string;
+  lastname: string;
+  github_profile_url: string;
+  username: string;
+}
+
+const TeamCard = ({ firstname, lastname, github_profile_url, username }: TeamCardProps) => {
+  return (
+    <div className="max-w-xs container bg-white rounded-xl shadow-lg transform transition duration-500 hover:scale-105 hover:shadow-2xl">
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl mt-2 ml-4 font-bold text-gray-800 cursor-pointer hover:text-gray-900 transition duration-100 pt-5 pb-4">
+          {firstname}
+          <br />
+          {lastname}
+        </h1>
+        <a href={`https://github.com/${username}`} target="_blank">
+          <img src={github_logo} alt="" className="w-10 h-10 mr-5" />
+        </a>
+      </div>
+      <img
+        className="w-full cursor-pointer"
+        src={github_profile_url}
+        alt={firstname+" "+lastname+" github profile image"}
+      />
+    </div>
+  );
+}
 
 export default AboutPage;
