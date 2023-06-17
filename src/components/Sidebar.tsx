7120f86f8d94ecc8a6d18ca8649b412a129e0463
Salipa-Gurung <salipagurung@gmail.com>
2023-06-17 13:45:33 +0545

    update: add project name to sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -20,15 +20,16 @@ export default function Sidebar() {
     return (
     <div className="col-span-1 p-5 h-screen border-r font-quicksand flex flex-col justify-between tracking-wide">
         <div>
-            <span>
+            <span className="flex items-center gap-2">
                 <img src={polaris_logo} alt="polaris suite logo" />
+                <p className="text-3xl font-bold font-sen">Polaris Suite</p>
             </span>
             <nav className="mt-10">
                 <ul>
                     {
                         sidebar.map((item, index) => (
                             <Link key={index} to={`/polaris/${item.link}`}>
-                                <li className={`py-3 px-2 rounded-md font-bold ${location.pathname.split("/")[2] === item.link && "bg-primary text-white"}`}>
+                                <li className={`py-3 px-2 my-4 rounded-md font-bold ${location.pathname.split("/")[2] === item.link && "bg-primary text-white"}`}>
                                     {item.title}
                                 </li>
                             </Link>
