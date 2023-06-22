60c20214696a64c4bc2a9fa21d5f59e8a6bc2c35
alsongarbuja <garbuja12alson@gmail.com>
2023-06-22 11:30:40 +0545

    CRUD: add delete and edit feature for environments

--- src/components/portal/EnvironmentModel.tsx
+++ src/components/portal/EnvironmentModel.tsx
@@ -9,19 +9,15 @@ interface IEnvironmentModelProps {
     closeModel: () => void;
     setEnvironments: React.Dispatch<React.SetStateAction<Partial<Project> | null>>;
     projectId: string;
+    environmentData: Partial<Environment> | undefined;
 }
 
-const EnvironmentModel = ({ closeModel, setEnvironments, projectId }: IEnvironmentModelProps) => {
+const EnvironmentModel = ({ closeModel, setEnvironments, projectId, environmentData }: IEnvironmentModelProps) => {
   
     const [environment, setEnvironment] = useState<Partial<Environment>>({
-        name: '',
-        description: '',
-        variables: [
-            {
-                name: '',
-                value: '',
-            }
-        ],
+        name: environmentData?.name || '',
+        description: environmentData?.description || '',
+        variables: environmentData?.variables || [],
     })
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setEnvironment(prev => ({
@@ -42,16 +38,43 @@ const EnvironmentModel = ({ closeModel, setEnvironments, projectId }: IEnvironme
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        const { statusCode, data, error } = await APICaller(`/environments/${projectId}`, "POST", {
-            ...environment,
-        });
+        let toSendEnvironment: Partial<Environment> = {
+            name: environment.name,
+        };
+
+        if(environment.description !== '') {
+            toSendEnvironment = {
+                ...toSendEnvironment,
+                description: environment.description,
+            }
+        }
+        if(environment.variables?.length !== 0) {
+            toSendEnvironment = {
+                ...toSendEnvironment,
+                variables: environment.variables?.map(env => ({ name: env.name, value: env.value })),
+            }
+        }
+
+        let url = `/environments/${projectId}`;
+        let method: "POST" | "PATCH" = "POST";
+
+        if(environmentData) {
+            url += `/${environmentData._id}`;
+            method = "PATCH";
+        }
+
+        const { statusCode, data, error } = await APICaller(url, method, toSendEnvironment);
         
         if(statusCode === 201) {
-
             setEnvironments(prev => ({
                 ...prev,
                 environments: [...prev?.environments!, data.environment],
             }));
+        } else if(statusCode === 200) {
+            setEnvironments(prev => ({
+                ...prev,
+                environments: prev?.environments?.map(env => env._id === environmentData?._id ? data.environment : env),
+            }));  
         } else {
             console.log(error);
         }
@@ -96,6 +119,7 @@ const EnvironmentModel = ({ closeModel, setEnvironments, projectId }: IEnvironme
                             }}
                             value={variable.name}
                             placeholder="Variable Name"
+                            required={false}
                         />
                         <br />
                         <Input 
@@ -109,6 +133,7 @@ const EnvironmentModel = ({ closeModel, setEnvironments, projectId }: IEnvironme
                             }}
                             value={variable.value}
                             placeholder="Variable Value"
+                            required={false}
                         />
                         <Button type="button" variant="dark" onClick={()=>removeVariable(index)}><Minus /></Button>
                       </div>  
@@ -117,7 +142,7 @@ const EnvironmentModel = ({ closeModel, setEnvironments, projectId }: IEnvironme
                 <div className="flex justify-end gap-2 mt-4">
                     <Button type="button" onClick={addVariables} variant="default" classes="rounded-sm">Add Variables</Button>
                     <Button type="button" onClick={closeModel} variant="default" classes="bg-red-500 rounded-sm">Cancel</Button>
-                    <button className="px-3 py-1 bg-green-500 text-white rounded-sm" type="submit">Create</button>
+                    <button className="px-3 py-1 bg-green-500 text-white rounded-sm" type="submit">{environmentData ? 'Edit' : 'Create'}</button>
                 </div>
             </form>
         </div>
--- src/pages/(protected)/projects/project.tsx
+++ src/pages/(protected)/projects/project.tsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react"
 import { APICaller } from "../../../helpers/api"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import Button from "../../../components/Button"
-import { Plus } from "react-feather"
+import { ArrowLeft, Plus } from "react-feather"
 import EnvironmentModel from "../../../components/portal/EnvironmentModel"
 
 const SingleProject = () => {
 
-    const [project, setProject] = useState<Partial<Project> | null>(null)
     const { projectId } = useParams();
-    const [showModel, setShowModel] = useState(false)
+    const navigate = useNavigate();
+    const [project, setProject] = useState<Partial<Project> | null>(null);
+    const [showModel, setShowModel] = useState(false);
+    const [selectedEnvironment, setSelectedEnvironment] = useState<Partial<Environment> | undefined>(undefined);
 
     useEffect(() => {
         (async () => {
@@ -23,11 +25,39 @@ const SingleProject = () => {
         })()
     }, [])
 
+    const handleDelete = async (id: string) => {
+        if(window.confirm("Are you sure you want to delete this environment?") === false) return
+
+        const { statusCode, error } = await APICaller(`/environments/${projectId}/${id}`, "DELETE")
+
+        if(statusCode === 200) {
+            setProject(prev => ({
+                ...prev,
+                environments: prev?.environments?.filter(env => env._id !== id)
+            }))
+        } else {
+            console.log(error)
+        }
+    }
+
+    const handleEditOpen = (id: string) => {
+        const environment = project?.environments?.find(env => env._id === id)
+        setSelectedEnvironment(environment)
+        setShowModel(true)
+    }
+    const handleCreateOpen = () => {
+        setSelectedEnvironment(undefined)
+        setShowModel(true)
+    }
+
   return (
     <main>
         <div className="flex justify-between items-center">
-            <h1>{project?.name}</h1>
-            <Button variant="primary" onClick={()=>setShowModel(true)}>
+            <div className="flex gap-2 items-center">
+                <ArrowLeft className="cursor-pointer" onClick={()=>navigate(-1)} />  
+                <h1>{project?.name}</h1>
+            </div>
+            <Button variant="primary" onClick={handleCreateOpen}>
                 <span className="flex gap-2">
                     <Plus />
                     Create Environment
@@ -50,11 +80,14 @@ const SingleProject = () => {
                         project?.environments?.map((environment) => (
                             <tr key={environment._id} className="border-b-2">
                                 <td className="p-3">{environment.name}</td>
-                                <td className="p-3">
-                                    <Button variant="default" onClick={()=>{}} classes="px-3 py-1 mr-3 bg-blue-500 text-white rounded-sm">
+                                <td className="p-3 flex items-center gap-2">
+                                    <Button variant="default" onClick={()=>{}} classes="px-3 py-1 bg-green-500 text-white rounded-sm">
+                                        View
+                                    </Button>
+                                    <Button variant="default" onClick={()=>handleEditOpen(environment._id!)} classes="px-3 py-1 bg-blue-500 text-white rounded-sm">
                                         Edit
                                     </Button>
-                                    <Button variant="default" onClick={()=>{}} classes="px-3 py-1 bg-red-500 text-white rounded-sm">
+                                    <Button variant="default" onClick={()=>handleDelete(environment._id!)} classes="px-3 py-1 bg-red-500 text-white rounded-sm">
                                         Delete
                                     </Button>
                                 </td>
@@ -64,7 +97,7 @@ const SingleProject = () => {
                 </tbody>
             </table>
         </div>
-        {showModel && <EnvironmentModel projectId={projectId!} closeModel={()=>setShowModel(false)} setEnvironments={setProject} />}
+        {showModel && <EnvironmentModel projectId={projectId!} environmentData={selectedEnvironment} closeModel={()=>setShowModel(false)} setEnvironments={setProject} />}
     </main>
   )
 }
