144b0254edd55e0f7cb059d82e256b84fac5b2b0
Salipa-Gurung <salipagurung@gmail.com>
2023-07-12 23:36:03 +0545

    layout: new layout for protected routes

--- src/layouts/ProtectedLayout.tsx
+++ src/layouts/ProtectedLayout.tsx
@@ -1,14 +1,24 @@
 import { PropsWithChildren } from "react";
 import Sidebar from "../components/Sidebar";
+import { polaris_logo } from "../assets/images";
 
 interface IProtectedLayoutProps {}
 
 const ProtectedLayout = ({ children }: PropsWithChildren<IProtectedLayoutProps>) => {
   return (
-    <main className="grid grid-cols-5">
-      <Sidebar />
-      <div className="col-span-4 p-5 h-screen overflow-y-scroll bg-gray-100">
-        {children}
+    <main >
+      <header className="bg-neutral_white flex justify-between items-center p-4 border-b">
+        <span className="font-bold flex items-center gap-2">
+          <img src={polaris_logo} alt="polaris suite logo" className="w-12 h-12" />
+          Poalris Suite
+        </span>
+        <span className="w-12 h-12 rounded-full bg-neutral_gray"></span>
+      </header>
+      <div className="flex justify-center">
+        <Sidebar />
+        <div className="p-5 w-full min-h-[85vh]">
+          {children}
+        </div>
       </div>
     </main>
   );
