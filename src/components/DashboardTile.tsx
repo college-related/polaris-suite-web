651903716920c82390ecce45732bc84c897e7d83
Salipa-Gurung <salipagurung@gmail.com>
2023-07-12 23:24:01 +0545

    ui: new dashboard UI

--- src/components/DashboardTile.tsx
+++ src/components/DashboardTile.tsx
@@ -1,23 +1,16 @@
 interface IDashboardTileProps {
-    title: string;
-    value: number;
-    icon: JSX.Element;
+  title: string;
+  value: number;
+  icon: JSX.Element;
 }
 
 const DashboardTile = ({ title, value, icon }: IDashboardTileProps) => {
   return (
-    <div className=" p-5 bg-white border rounded shadow">
-      <div className="w-full h-32 flex items-center">
-        <div className="flex-shrink pr-4">
-          <div className="rounded p-3 bg-green-600 text-white">
-              {icon}
-          </div>
-        </div>
-        <div className="flex-1 text-right md:text-center">
-          <h5 className="font-bold uppercase text-gray-400">{title}</h5>
-          <h3 className="font-bold text-3xl text-gray-600">{value}</h3>
-        </div>
+    <div className="p-5 bg-neutral_white rounded">
+      <div className="inline-block rounded p-3 bg-deep_blue text-white my-2">
+        {icon}
       </div>
+      <h5 className="font-bold text-xl text-dark">{value} {title}</h5>
     </div>
   )
 }
--- src/pages/(protected)/Dashboard.tsx
+++ src/pages/(protected)/Dashboard.tsx
@@ -1,22 +1,57 @@
-import { File, HardDrive, Layers } from "react-feather"
+import { Download, DownloadCloud, Edit3, File, HardDrive, Layers } from "react-feather"
 
 import { useState } from "react"
 import { getUser } from "../../helpers/cookie"
 
 import DashboardTile from "../../components/DashboardTile"
+import IconButton from "../../components/IconButton"
 
 const Dashboard = () => {
   const [user] = useState(getUser().name)
+  const [shortcuts, setShortcuts] = useState([
+    { title: "Polaris Suite", icon: <DownloadCloud /> },
+    { title: "E Guru", icon: <DownloadCloud /> },
+    { title: "Rails", icon: <DownloadCloud /> },
+  ]);
 
   return (
     <div className="p-5">
-        <h1 className="mb-10">Welcome, {user}</h1>
+      <h1 className="mb-4">Welcome, {user}</h1>
 
-        <div className="grid gap-2 grid-cols-1 md:grid-cols-3">
-          <DashboardTile title="Total Projects" value={5} icon={<HardDrive />} />
-          <DashboardTile title="Total Reports" value={3} icon={<File />} />
-          <DashboardTile title="Total Test Cases" value={30} icon={<Layers />} />
-        </div>
+      <div className="grid gap-2 grid-cols-1 md:grid-cols-3">
+        <DashboardTile title="Total Projects" value={5} icon={<HardDrive />} />
+        <DashboardTile title="Total Reports" value={3} icon={<File />} />
+        <DashboardTile title="Total Test Cases" value={30} icon={<Layers />} />
+      </div>
+
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 mt-8">
+        <section>
+          <h2>Recent Activities</h2>
+          
+        </section>
+        <section>
+          <div className="flex justify-between items-start">
+            <h2>Shortcuts</h2>
+            <IconButton onClick={()=>{}} variant="clear" icon={<Edit3 className="w-5 h-5" />} />
+          </div>
+          {
+            shortcuts.length > 0 ? (
+              <>
+                {
+                  shortcuts.map((shortcut, index) => (
+                    <div className="bg-deep_blue p-4 rounded-md my-4 text-white flex gap-6" key={index}>
+                      {shortcut.icon}
+                      <h3>{shortcut.title}</h3>
+                    </div>
+                  ))
+                }
+              </>
+            ) : (
+              <p className="text-center">No shortcuts yet.</p>
+            )
+          }
+        </section>
+      </div>
     </div>
   )
 }
