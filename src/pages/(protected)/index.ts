d14cc5d3af3c35f2f5bae8e40c2f2f51262180cc
alsongarbuja <garbuja12alson@gmail.com>
2023-07-13 22:58:30 +0545

    react: use barrel export for pages

---
+++ src/pages/(protected)/index.ts
@@ -0,0 +1,10 @@
+export { default as Dashboard } from "./Dashboard";
+export { default as Settings } from "./Settings";
+
+export { default as ProjectsPage } from "./projects/index";
+export { default as SingleProject } from "./projects/project";
+export { default as TestCases } from "./projects/TestCases";
+export { default as ProjectSettingPage } from "./projects/Settings";
+export { default as ProjectActivityPage } from "./projects/Activities";
+
+// Path: src\pages\(protected)\Dashboard\index.ts
