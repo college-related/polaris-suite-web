b16f4e264dd9e64d097250309448603fb37e5517
alsongarbuja <garbuja12alson@gmail.com>
2023-07-15 12:20:13 +0545

    component: update the select custom component

--- src/components/form/Select.tsx
+++ src/components/form/Select.tsx
@@ -1,39 +1,58 @@
-
 interface ISelectProps {
-    label: string;
-    name: string;
-    onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
-    options: dynamicObjectString[];
-    required?: boolean;
-    disabled?: boolean;
-    classes?: string;
-    errors?: dynamicObjectString | null;
-    [key: string]: any;
+  label: string;
+  name: string;
+  onChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => void;
+  options: dynamicObjectString[];
+  required?: boolean;
+  disabled?: boolean;
+  classes?: string;
+  errors?: dynamicObjectString | null;
+  value?: string;
+  [key: string]: any;
 }
 
 const Select = (props: ISelectProps) => {
-
-    const { label, name, onChange, options, required, disabled, classes, errors, ...rest } = props;
+  const {
+    label,
+    name,
+    onChange,
+    options,
+    required,
+    disabled,
+    classes,
+    value,
+    errors,
+    ...rest
+  } = props;
 
   return (
     <div className={`${classes}`}>
-        <label htmlFor={label} className="text-gray-700 font-bold font-quicksand">{label}</label>
-        <select 
-            name={name} 
-            id={label} 
-            disabled={disabled} 
-            onChange={onChange} 
-            className={`w-full p-3 rounded-sm border mt-2 ${(errors && errors.hasOwnProperty(name)) && 'border-red-500'} ${classes}`}
-        >
-            {options.map((option, index) => (
-                <option key={index} value={option.value}>{option.name}</option>
-            ))}
-        </select>
-        {(errors && errors.hasOwnProperty(name)) && (
-            <p className="text-red-500 text-sm">{errors[name]}</p>
-        )}
+      <label htmlFor={label} className="text-gray-700 font-bold font-quicksand">
+        {label}
+      </label>
+      <select
+        name={name}
+        id={label}
+        value={value}
+        disabled={disabled}
+        onChange={onChange}
+        className={`w-full p-3 rounded-sm border mt-2 ${
+          errors && errors.hasOwnProperty(name) && "border-red-500"
+        } ${classes}`}
+      >
+        {options.map((option, index) => (
+          <option key={index} value={option.value}>
+            {option.name}
+          </option>
+        ))}
+      </select>
+      {errors && errors.hasOwnProperty(name) && (
+        <p className="text-red-500 text-sm">{errors[name]}</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Select
+export default Select;
