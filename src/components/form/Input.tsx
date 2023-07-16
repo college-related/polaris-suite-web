e73848ede7ed421ed33f77f636671a4aac251720
alsongarbuja <garbuja12alson@gmail.com>
2023-07-17 00:52:18 +0545

    package-component: add uuid pacakage and use as id for label in input component

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -1,47 +1,54 @@
+import { useState } from "react";
+import { v4 } from "uuid";
 
 interface IInputProps {
-    label: string;
-    type?: "text" | "number" | "email" | "password" | "date" | "time";
-    value: string | number;
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    placeholder?: string;
-    required?: boolean;
-    disabled?: boolean;
-    classes?: string;
-    errors?: dynamicObjectString | null;
-    [key: string]: any;
+  label: string;
+  type?: "text" | "number" | "email" | "password" | "date" | "time";
+  value: string | number;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  required?: boolean;
+  disabled?: boolean;
+  classes?: string;
+  inpClasses?: string;
+  errors?: dynamicObjectString | null;
+  [key: string]: any;
 }
 
 const Input = ({ 
-    label, 
-    name,
-    type="text", 
-    value, 
-    onChange, 
-    placeholder="", 
-    required=true, 
-    disabled=false, 
-    classes="", 
-    errors=null,
-    ...rest 
+  label, 
+  name,
+  type="text", 
+  value, 
+  onChange, 
+  placeholder="", 
+  required=true, 
+  disabled=false, 
+  classes="", 
+  inpClasses="",
+  errors=null,
+  ...rest 
 }: IInputProps) => {
+  const [id] = useState<string>(name+v4());
+  
   return (
     <div className={`${classes}`}>
-        <label htmlFor={name} className="text-gray-700 font-bold font-quicksand">{label}</label>
+      <label htmlFor={id} className="text-gray-700 font-bold font-quicksand">{label}</label>
         <input 
-            id={name}
-            type={type}
-            value={value}
-            name={name}
-            onChange={onChange}
-            placeholder={placeholder}
-            required={required}
-            disabled={disabled}
-            className={`w-full p-3 rounded-sm border mt-2 ${(errors && errors.hasOwnProperty(name)) && 'border-red-500'}`}
-            {...rest}
+          id={id}
+          type={type}
+          value={value}
+          name={name}
+          onChange={onChange}
+          placeholder={placeholder}
+          required={required}
+          disabled={disabled}
+          autoComplete={type}
+          className={`w-full p-3 rounded-sm border mt-2 ${inpClasses} ${(errors && errors.hasOwnProperty(name)) && 'border-red-500'}`}
+          {...rest}
         />
         {(errors && errors.hasOwnProperty(name)) && (
-            <p className="text-red-500 text-sm">{errors[name]}</p>
+          <p className="text-red-500 text-sm">{errors[name]}</p>
         )}
     </div>
   )
